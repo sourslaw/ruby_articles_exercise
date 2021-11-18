5fa7257ac944a31ea23d8ff57e106f0e46477e65
Scott Lawan <scottlawan@Scotts-Mac-mini.local>
2021-11-18 11:06:45 -0700

    adding one to many associations for users/articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new( article_params ) 
+    @article.user = User.first
+    
     if @article.save
       flash[:notice] = "article was created successfully" # flash helper to signal successful posting of new article
       redirect_to @article # what do after the save. redirect to the article that was recreated
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { minimum: 6, maximum: 100 } # makes sure title is present in order for articl to be saved
+
   validates :description, presence: true, length: { minimum: 10, maximum: 350 }
+  
 end
